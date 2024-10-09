e2b4a06c84ce4d2e7a0cbaaf4d6a5e157d837d7c
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2024-10-10 02:06:41 +0300

    adding docs

--- api.go
+++ api.go
@@ -26,7 +26,12 @@ func NewAPIServer(listenAddr string, store Storage) *APIServer {
 	}
 }
 
-// Handle The Account Routes
+// handleAccount handles HTTP requests for account-related operations.
+// It delegates the request to the appropriate handler based on the HTTP method:
+// - GET: Calls handleGetAccounts to retrieve account information.
+// - POST: Calls handleCreateAccount to create a new account.
+// - DELETE: Calls handleDeleteAccount to delete an existing account.
+// If the HTTP method is not supported, it returns an error indicating that the method is not allowed.
 func (as *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
 	// Check The Method and Call The Right Handler
 	switch r.Method {
@@ -41,8 +46,16 @@ func (as *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error
 	}
 }
 
-// Handle The Get Accounts Route
-// Return All Accounts
+// handleGetAccounts handles the HTTP request to retrieve all accounts.
+// It fetches all accounts from the store and writes them as a JSON response.
+// If an error occurs while fetching the accounts, it returns the error.
+//
+// Parameters:
+//   - w: The HTTP response writer to send the response.
+//   - r: The HTTP request received from the client.
+//
+// Returns:
+//   - error: An error if there is an issue retrieving the accounts or writing the response.
 func (as *APIServer) handleGetAccounts(w http.ResponseWriter, r *http.Request) error {
 	// Get All Accounts From The Store
 	accs, err := as.store.GetAccounts()
@@ -54,7 +67,17 @@ func (as *APIServer) handleGetAccounts(w http.ResponseWriter, r *http.Request) e
 	return WriteJSON(w, http.StatusOK, accs)
 }
 
-// Handle The Create Account Route
+// handleCreateAccount handles the creation of a new account.
+// It decodes the request body into a CreateAccountRequest, creates a new account,
+// stores it in the database, and writes the created account as a JSON response.
+//
+// Parameters:
+//   - w: http.ResponseWriter to write the response.
+//   - r: *http.Request containing the request data.
+//
+// Returns:
+//   - error: An error if the request body cannot be decoded, the account cannot be created,
+//     or the account cannot be stored in the database.
 func (as *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
 	// Decode The Request Body To CreateAccountRequest
 	accReq := new(CreateAccountRequest)
@@ -75,7 +98,16 @@ func (as *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request)
 	return WriteJSON(w, http.StatusCreated, acc)
 }
 
-// Handle The Get Account By ID Route
+// handleGetAccountById handles the HTTP request to retrieve an account by its ID.
+// It extracts the account ID from the URL, fetches the account details from the store,
+// and writes the account information as a JSON response.
+//
+// Parameters:
+//   - w: http.ResponseWriter to write the response.
+//   - r: *http.Request containing the request details.
+//
+// Returns:
+//   - error: An error if the account retrieval fails, otherwise nil.
 func (as *APIServer) handleGetAccountById(w http.ResponseWriter, r *http.Request) error {
 	// Get The ID From The URL
 	id := getId(w, r)
@@ -90,7 +122,16 @@ func (as *APIServer) handleGetAccountById(w http.ResponseWriter, r *http.Request
 	return WriteJSON(w, http.StatusOK, acc)
 }
 
-// Handle The Delete Account Route
+// handleDeleteAccount handles the HTTP request for deleting an account.
+// It extracts the account ID from the URL, deletes the account from the store,
+// and writes a JSON response indicating the deleted account ID.
+//
+// Parameters:
+//   - w: http.ResponseWriter to write the HTTP response.
+//   - r: *http.Request containing the HTTP request details.
+//
+// Returns:
+//   - error: An error if the account deletion fails, otherwise nil.
 func (as *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
 	// Get The ID From The URL
 	id := getId(w, r)
@@ -105,7 +146,17 @@ func (as *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request)
 	})
 }
 
-// Handle The Transfer Route
+// handleTransfer handles the transfer request by decoding the JSON payload
+// from the request body into a TransferRequest struct. It returns an error
+// if the decoding fails. The function writes the transfer request back as a
+// JSON response with an HTTP status of 200 OK.
+//
+// Parameters:
+// - w: http.ResponseWriter to write the response.
+// - r: *http.Request containing the transfer request.
+//
+// Returns:
+// - error: An error if the JSON decoding fails.
 func (as *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	transferReq := TransferRequest{}
 
@@ -124,7 +175,16 @@ func (as *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) erro
 	return WriteJSON(w, http.StatusOK, transferReq)
 }
 
-// Run The API Server
+// Run initializes the API server, sets up the router and sub-router with the appropriate
+// routes for handling account and transfer operations, and starts the HTTP server.
+//
+// Routes:
+// - POST /api/v1/account: Handles account creation.
+// - GET /api/v1/account/{id:[0-9]+}: Retrieves account details by ID.
+// - DELETE /api/v1/account/{id:[0-9]+}: Deletes an account by ID.
+// - POST /api/v1/transfer: Handles money transfers between accounts.
+//
+// The server listens on the address specified in the APIServer's listenAddr field.
 func (as *APIServer) Run() {
 	// Create The Router and SubRouter
 	router := mux.NewRouter()
@@ -140,26 +200,33 @@ func (as *APIServer) Run() {
 
 	// Run The HTTPServer
 
-	log.Println("Server is Runing On Port: ", as.listenAddr)
+	log.Println("API Server is Runing On Port: ", as.listenAddr)
 
 	http.ListenAndServe(as.listenAddr, router)
 }
 
-/*
-API Helper Functions
-*/
+// apiFunc is a type definition for a function that takes an http.ResponseWriter
+// and an *http.Request as parameters and returns an error. This type is used
+// to define the signature of API handler functions in the application.
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
-/*
-API Error Response
-*/
+// APIError is a struct that represents an error response in the API.
+// It contains an "error" field that holds the error message to be returned
+// to the client in the response body.
 type APIError struct {
 	Error string `json:"error"`
 }
 
-/*
-Make HTTP Handler Function Wrapper
-*/
+// makeHTTPHandlerFunc wraps an apiFunc with an http.HandlerFunc.
+// It executes the provided apiFunc and handles any errors by writing
+// a JSON response with a status code of http.StatusBadRequest and an
+// APIError containing the error message.
+//
+// Parameters:
+//   - f: The apiFunc to be wrapped.
+//
+// Returns:
+//   - An http.HandlerFunc that executes the provided apiFunc and handles errors.
 func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
@@ -170,9 +237,16 @@ func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc {
 	}
 }
 
-/*
-Write JSON Response Utility
-*/
+// WriteJSON writes the given data as a JSON response with the specified HTTP status code.
+// It sets the "Content-Type" header to "application/json" and encodes the data into the response writer.
+//
+// Parameters:
+//   - w: The http.ResponseWriter to write the response to.
+//   - status: The HTTP status code to set for the response.
+//   - data: The data to encode and write as JSON.
+//
+// Returns:
+//   - error: An error if encoding the data fails, otherwise nil.
 func WriteJSON(w http.ResponseWriter, status int, data interface{}) error {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
@@ -180,9 +254,16 @@ func WriteJSON(w http.ResponseWriter, status int, data interface{}) error {
 	return json.NewEncoder(w).Encode(data)
 }
 
-/*
-Get ID From URL
-*/
+// getId extracts the "id" variable from the URL path, converts it to an integer,
+// and returns the integer value. If the conversion fails, it writes an error
+// message to the ResponseWriter and returns -1.
+//
+// Parameters:
+//   - w: http.ResponseWriter to write the error message if the conversion fails.
+//   - r: *http.Request from which the "id" variable is extracted.
+//
+// Returns:
+//   - int: The integer value of the "id" variable, or -1 if the conversion fails.
 func getId(w http.ResponseWriter, r *http.Request) int {
 	id := mux.Vars(r)["id"]
 
--- storage.go
+++ storage.go
@@ -10,6 +10,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Storage interface
+// Represents a storage interface that defines the methods for interacting with the database.
 type Storage interface {
 	CreateAccount(*Account) error
 	DeleteAccount(int) error
@@ -18,10 +20,22 @@ type Storage interface {
 	GetAccountById(int) (*Account, error)
 }
 
+// PostgresStorage struct
+// Represents a PostgreSQL storage implementation.
 type PostgresStorage struct {
 	db *sql.DB
 }
 
+// NewPostgresStorage initializes a new PostgresStorage instance by loading
+// environment variables from a .env file and establishing a connection to
+// the PostgreSQL database using the connection string specified in the DB_URL
+// environment variable. It returns a pointer to the PostgresStorage instance
+// and an error if any occurs during the process.
+//
+// Returns:
+//   - *PostgresStorage: A pointer to the initialized PostgresStorage instance.
+//   - error: An error if there is an issue loading the .env file, opening the
+//     database connection, or pinging the database.
 func NewPostgresStorage() (*PostgresStorage, error) {
 	err := godotenv.Load()
 	if err != nil {
@@ -46,7 +60,11 @@ func NewPostgresStorage() (*PostgresStorage, error) {
 	}, nil
 }
 
+// Init initializes the PostgresStorage by creating the accounts table if it does not already exist.
+// The table includes columns for id, first_name, last_name, number, balance, and create_at.
+// If there is an error during table creation, the function logs a fatal error.
 func (s *PostgresStorage) Init() {
+	// Create The Table
 	_, err := s.db.Exec(`CREATE TABLE IF NOT EXISTS accounts (
 		id SERIAL PRIMARY KEY,
 		first_name TEXT,
@@ -61,7 +79,16 @@ func (s *PostgresStorage) Init() {
 	}
 }
 
+// GetAccounts retrieves all accounts from the Postgres database.
+// It executes a SQL query to select all records from the 'accounts' table,
+// scans each row into an Account struct, and returns a slice of Account pointers.
+// If an error occurs during the query or scanning process, it returns the error.
+//
+// Returns:
+//   - []*Account: A slice of pointers to Account structs representing the accounts.
+//   - error: An error object if an error occurs, otherwise nil.
 func (s *PostgresStorage) GetAccounts() ([]*Account, error) {
+	// Query The Database
 	rows, err := s.db.Query(`SELECT * FROM accounts`)
 
 	if err != nil {
@@ -80,6 +107,14 @@ func (s *PostgresStorage) GetAccounts() ([]*Account, error) {
 	return accounts, nil
 }
 
+// CreateAccount inserts a new account record into the accounts table in the database.
+// It takes an Account struct as input and returns an error if the insertion fails.
+//
+// Parameters:
+//   - account: A pointer to an Account struct containing the account details to be inserted.
+//
+// Returns:
+//   - error: An error object if the insertion fails, otherwise nil.
 func (s *PostgresStorage) CreateAccount(account *Account) error {
 	_, err := s.db.Exec(`INSERT INTO accounts (
 	first_name,
@@ -95,6 +130,14 @@ func (s *PostgresStorage) CreateAccount(account *Account) error {
 	return nil
 }
 
+// DeleteAccount deletes an account from the database based on the provided account ID.
+// It returns an error if the account is not found or if there is an issue with the database query.
+//
+// Parameters:
+//   - id: The ID of the account to be deleted.
+//
+// Returns:
+//   - error: An error object if the account is not found or if there is a database query issue, otherwise nil.
 func (s *PostgresStorage) DeleteAccount(id int) error {
 	_, err := s.db.Query(`DELETE FROM accounts WHERE id = $1`, id)
 
@@ -105,10 +148,21 @@ func (s *PostgresStorage) DeleteAccount(id int) error {
 	return nil
 }
 
+// TODO: Implement UpdateAccount method
 func (s *PostgresStorage) UpdateAccount(account *Account) error {
 	return nil
 }
 
+// GetAccountById retrieves an account from the database based on the provided account ID.
+// It executes a SQL query to select the account with the specified ID, scans the row into an Account struct,
+// and returns a pointer to the Account struct if the account is found. If the account is not found, it returns an error.
+//
+// Parameters:
+//   - id: The ID of the account to retrieve.
+//
+// Returns:
+//   - *Account: A pointer to the Account struct representing the account.
+//   - error: An error object if the account is not found, otherwise nil.
 func (s *PostgresStorage) GetAccountById(id int) (*Account, error) {
 	rows, err := s.db.Query(`SELECT * FROM accounts WHERE id = $1`, id)
 
@@ -123,6 +177,15 @@ func (s *PostgresStorage) GetAccountById(id int) (*Account, error) {
 	return nil, fmt.Errorf("account %d not found", id)
 }
 
+// scanIntoAccount scans the current row of the provided SQL rows object into an Account struct.
+// It returns a pointer to the Account struct and an error if the scanning process fails.
+//
+// Parameters:
+//   - row: A pointer to the SQL rows object representing the current row.
+//
+// Returns:
+//   - *Account: A pointer to the Account struct containing the scanned data.
+//   - error: An error object if the scanning process fails, otherwise nil.
 func scanIntoAccount(row *sql.Rows) (*Account, error) {
 	account := &Account{}
 	if err := row.Scan(&account.ID, &account.FirstName, &account.LastName, &account.Number, &account.Balance, &account.CreatedAt); err != nil {
